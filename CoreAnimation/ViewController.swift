10f347ac968481fbe0bd78efba34f39e4d5fbe31
TheRenat <therenat@mail.ru>
2022-08-05 13:45:27 +0300

    add logic

--- CoreAnimation/ViewController.swift
+++ CoreAnimation/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var springAnimationView: SpringView!
+    @IBOutlet var animationButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //animationButton.configuration?.title = "START ANIMATION"
     }
-
-
+    
+    @IBAction func startAnimationButton(_ sender: UIButton) {
+        get(animation: nextAnimation())
+                //animationButton.configuration?.title ?? "")
+        print(springAnimationView.animation)
+    }
+    
+    private func get(animation: String) {
+        springAnimationView.animation = animation
+        springAnimationView.curve = AnimationCurve.allCases.randomElement()?.rawValue ?? " "
+        springAnimationView.delay = Double.random(in: 1...1.5)
+        springAnimationView.force = Double.random(in: 0...1)
+        springAnimationView.duration = Double.random(in: 0...1)
+        springAnimationView.animate()
+        nextAnimation()
+    }
+    
+    private func nextAnimation() -> String {
+        let number = AnimationPreset.allCases.randomElement()?.rawValue ?? " "
+        animationButton.configuration?.title = number
+        return number
+    }
 }
 
