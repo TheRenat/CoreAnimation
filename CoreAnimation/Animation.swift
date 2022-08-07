bbd91286791e53da4fa61d4ac56d6dfe730ec852
TheRenat <therenat@mail.ru>
2022-08-07 23:48:02 +0300

    add DataStore

---
+++ CoreAnimation/Animation.swift
@@ -0,0 +1,12 @@
+//
+//  Animation.swift
+//  CoreAnimation
+//
+//  Created by Renat Ibragimov on 07.08.2022.
+//
+
+import Foundation
+
+struct Animaton {
+    
+}
---
+++ CoreAnimation/DataStore.swift
@@ -0,0 +1,14 @@
+//
+//  DataStore.swift
+//  CoreAnimation
+//
+//  Created by Renat Ibragimov on 07.08.2022.
+//
+
+import Foundation
+import SpringAnimation
+
+class DataStore {
+    
+    let animations = AnimationPreset.allCases
+}
