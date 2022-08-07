ca2e86d240b86225fb27f4ce691db823b98107e5
TheRenat <therenat@mail.ru>
2022-08-07 23:51:43 +0300

    add model

--- CoreAnimation/Animation.swift
+++ CoreAnimation/Animation.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 struct Animaton {
+    let name: String
+    let curve: String
+    let force: Int
+    let duration: Int
+    let delay: Int
     
+    var description: String {
+        """
+        preset: \(name)
+        curve: \(curve)
+        force: \(force)
+        duration: \(duration)
+        delay: \(delay)
+        """
+    }
+    
+    func getRandomAnimation() {
+        Animaton(
+            name: <#T##String#>, curve: <#T##String#>, force: <#T##Int#>, duration: <#T##Int#>, delay: <#T##Int#>)
+    }
 }
